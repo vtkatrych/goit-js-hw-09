b1386add4564d4d989368846f2022fced2bbdb4c
Vitalii <katrychvitalii23@gmail.com>
2026-08-15 19:38:23 +0300

    other 50%

---
+++ src/js/2-form.js
@@ -0,0 +1,44 @@
+const formData = {
+  email: '',
+  message: '',
+};
+
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  const fieldName = event.target.name;
+  const fieldValue = event.target.value.trim();
+  formData[fieldName] = fieldValue;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+const savedSettings = localStorage.getItem('feedback-form-state');
+if (savedSettings) {
+  const parsedData = JSON.parse(savedSettings);
+  formData = parsedData;
+
+  form.elements.email.value = parsedData.email;
+  form.elements.message.value = parsedData.message;
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (formData.email === '' || formData.message === '') {
+    alert('Fill please all fields');
+    return;
+  }
+  console.log(formData);
+
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+  formData = {
+    email: '',
+    message: '',
+  };
+}
